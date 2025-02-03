e469b2ad96fd17a3146facf999b40445456ec6e7
Nehal409 <nehalnasir619@gmail.com>
2025-02-03 18:14:33 +0500

    chat header and message input with integrating send message API

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
   isLoggingIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
+  onlineUsers: [],
 
   // Check authentication and fetch user profile
   checkAuth: async () => {
--- src/store/useChatStore.js
+++ src/store/useChatStore.js
@@ -2,7 +2,7 @@ import toast from "react-hot-toast";
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -41,6 +41,30 @@ export const useChatStore = create((set) => ({
       toast.error(error?.response?.data?.error.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const accessToken = localStorage.getItem("accessToken");
+    const { selectedUser, messages } = get();
+    try {
+      console.log(messageData);
+      
+      const res = await axiosInstance.post(
+        `/messages/users/${selectedUser._id}`,
+        messageData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log(res);
+      console.log(res.data.data.messages.text);
+
+      set({ messages: [...messages, res.data.data.messages.text] }); // Keep all the previous messages and add the very last one message to the end.
+    } catch (error) {
+      toast.error(error?.response?.data?.error.message);
     }
   },
 
